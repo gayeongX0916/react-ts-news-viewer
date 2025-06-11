2e6656f750005a11580e0931cc1e5cda240778f9
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-11 15:09:04 +0900

    [feature] 로딩 및 에러 관리

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import type { NewsData } from "./types/NewsData";
 function App() {
   const [articles, setAricles] = useState<NewsData[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("all");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleClick = (item: string) => {
     setSelectedCategory(item);
@@ -15,13 +17,24 @@ function App() {
 
   useEffect(() => {
     const query = selectedCategory === "all" ? "" : selectedCategory;
-    fetchNewsData(query).then((data) => {
-      setAricles(data);
-    });
+    fetchNewsData(query)
+      .then((data) => {
+        setLoading(true);
+        setError(null);
+        setAricles(data);
+      })
+      .catch(() => {
+        setError("뉴스를 불러오는 중 문제가 발생했습니다.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [selectedCategory]);
 
   return (
     <div className="news-viewer">
+      {loading && <p>뉴스를 불러오는 중입니다...</p>}
+      {error && <p>{error}</p>}
       <Categories selectedCategory={selectedCategory} onSelect={handleClick} />
       <NewsList articles={articles} />
     </div>
